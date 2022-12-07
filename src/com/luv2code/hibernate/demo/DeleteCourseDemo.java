f8bee8241570e149192c9305c6234f2387e227b6
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2022-12-07 17:29:24 +0200

    hb-eager-vs-lazy-demo

--- src/com/luv2code/hibernate/demo/DeleteCourseDemo.java
+++ src/com/luv2code/hibernate/demo/DeleteCourseDemo.java
@@ -4,39 +4,49 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.luv2code.hibernate.demo.entity.Student;
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class CreateStudentDemo {
+public class DeleteCourseDemo {
 
 	public static void main(String[] args) {
 
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
 								.buildSessionFactory();
 		
 		// create session
 		Session session = factory.getCurrentSession();
 		
 		try {			
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
 			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
+			// get a course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
 			
+			// delete course
+			System.out.println("Deleting course: " + tempCourse);
+			
+			session.delete(tempCourse);
+
 			// commit transaction
 			session.getTransaction().commit();
 			
 			System.out.println("Done!");
 		}
 		finally {
+			
+			// add clean up code
+			session.close();
+			
 			factory.close();
 		}
 	}
